0c7d5decd3433c5402b2a2a442a39b5b30908260
seanMooney1996 <seanmooney1996@gmail.com>
2024-01-22 19:31:53 +0000

    Q8 almost completed

--- src/CA3_Question8.java
+++ src/CA3_Question8.java
@@ -1,7 +1,10 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
+
 /**
- *  Name:
- *  Class Group:
+ * Name:
+ * Class Group:
  */
 public class CA3_Question8 {
 
@@ -13,5 +16,65 @@ public class CA3_Question8 {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter equation");
         equation = in.nextLine().trim();
+
+        Deque<Integer> numberStack = new ArrayDeque<>();
+        Deque<Character> operatorStack = new ArrayDeque<>();
+
+        for (int i = 0; i < equation.length(); i++) {
+            char currentChar = equation.charAt(i);
+            int intValueOfChar = currentChar;
+            if (intValueOfChar > 47 && intValueOfChar < 58) {
+                numberStack.push(intValueOfChar);
+            } else if (currentChar == '(') {
+                operatorStack.push(currentChar);
+            } else if (currentChar == '+' || currentChar == '-' || currentChar == 'x' || currentChar == '/') {
+                int operatorPrecedenceTopOfStack;
+                int operatorPrecedenceNotOnStack;
+                if (!operatorStack.isEmpty()){
+                    char operatorTopOfStack = operatorStack.peek();
+                    if (operatorTopOfStack == 'x' || operatorTopOfStack == '/') {
+                        operatorPrecedenceTopOfStack = 2;
+                    } else {
+                        operatorPrecedenceTopOfStack = 1;
+                    }
+                    if (currentChar == 'x' || currentChar == '/') {
+                        operatorPrecedenceNotOnStack = 2;
+                    } else {
+                        operatorPrecedenceNotOnStack = 1;
+                    }
+
+                    if (operatorPrecedenceTopOfStack > operatorPrecedenceNotOnStack) {
+                        evaluateTop(numberStack,operatorStack);
+                    }
+                }
+
+            } else if (currentChar==')'){
+                if (operatorStack.peek()!='('){
+                    evaluateTop(numberStack,operatorStack);
+                }
+                operatorStack.pop();
+            }
+        }
+        while (!operatorStack.isEmpty()){
+            evaluateTop(numberStack,operatorStack);
+        }
+
+        System.out.println("Final number is = "+numberStack.pop());
+    }
+    public static void evaluateTop(Deque<Integer> numberStack,Deque<Character> operatorStack){
+        int numToGoOnStack;
+        int first = numberStack.pop();
+        int second = numberStack.pop();
+        char operator = operatorStack.pop();
+        if (operator == '+') {
+            numToGoOnStack = first + second;
+        } else if (operator == '-') {
+            numToGoOnStack = second - first;
+        } else if (operator == 'x') {
+            numToGoOnStack = first * second;
+        } else {
+            numToGoOnStack = second / first;
+        }
+        numberStack.push(numToGoOnStack);
     }
 }
